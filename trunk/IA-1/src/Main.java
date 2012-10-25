ffc6e2d26e3b6a773ec39d711e35ce91cd750c2a
ivan.bargallo <ivan.bargallo@4c882253-0f09-8658-30a3-2cef7b2bf161>
2012-10-25 16:59:24 +0000

    miau

--- trunk/IA-1/src/Main.java
+++ trunk/IA-1/src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
 	public static void main(String args[]) {
-		
+		int nServers = 50;
+		int nReplications = 5;
+		int nUsers = 200;
+		int nRequestsUser = 5;
+		int seed = 10;
+		Estat estat = new Estat(nServers, nReplications, nUsers,nRequestsUser,seed);
 	}
 }
