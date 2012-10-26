597c3e53c4aee5e7cb634a7860da2db811b9024a
ivan.bargallo <ivan.bargallo@4c882253-0f09-8658-30a3-2cef7b2bf161>
2012-10-26 15:59:29 +0000

    nya

--- trunk/IA-1/src/State.java
+++ trunk/IA-1/src/State.java
@@ -12,11 +12,11 @@ public class State {
 	private static Requests sRequests = null;
 	private static int sRequestsCount = 0;
 	private static int sServersCount = 0;
-	
+	private static int sPenalization = 5000;
+
 	// Parte dinamica
 	private Link[] mServersRequests = null;
 	private int[] mRequestsServers = null;
-	
 
 	public State(int nServers, int nReplications, int nUsers,
 			int nRequestsUser, int seed) {
@@ -33,7 +33,12 @@ public class State {
 		for (int i = 0; i < sRequestsCount; ++i) {
 			mRequestsServers[i] = -1;
 		}
-		// ESTADO INICIAL "FINAL VALIDO"
+	}
+
+	public State(int nServers, int nReplications, int nUsers,
+			int nRequestsUser, int seed, int penalization) {
+		this(nServers, nReplications, nUsers, nRequestsUser, seed);
+		sPenalization = penalization;
 	}
 
 	public void initialRandomStateFullRequests(int seed) {
@@ -43,49 +48,149 @@ public class State {
 		int idFile = -1;
 		int idServer = -1;
 		Iterator<Integer> it;
-		for(int i = 0; i < sRequestsCount; ++i){
+		for (int i = 0; i < sRequestsCount; ++i) {
 			idFile = sRequests.getRequest(i)[1];
-			//Obtener la lista de servidores con ese fichero
+			// Obtener la lista de servidores con ese fichero
 			serverSet = sServers.fileLocations(idFile);
-			
-			//Elegir aleatoriamente un servidor con el fichero
+
+			// Elegir aleatoriamente un servidor con el fichero
 			rnd = rndGr.nextInt(serverSet.size());
 			it = serverSet.iterator();
 			for (int j = 0; j <= rnd; ++j) {
 				idServer = it.next();
 			}
-			//Asignarle el fichero
-			//canSwap es implícito al haber buscado un válido
+			// Asignarle el fichero
+			// canSwap es implícito al haber buscado un válido
 			swapOperator(idServer, i);
-		}		
-	}
-	
-	public void initialGreedyStateFullRequests() {
-		
+		}
 	}
 
-	public void initialRandomState() {
-		
+	public void initialGreedyStateFullRequests() {
+		Set<Integer> serverSet;
+		int idFile = -1;
+		int idServer = -1;
+		int nextServer = -1;
+		int optimalTime = -1;
+
+		Iterator<Integer> it;
+		for (int i = 0; i < sRequestsCount; ++i) {
+			idFile = sRequests.getRequest(i)[1];
+			// Obtener la lista de servidores con ese fichero
+			serverSet = sServers.fileLocations(idFile);
+
+			// Elegir el servidor con menos carga
+			it = serverSet.iterator();
+			idServer = it.next(); // it tendrá mínimo un servidor, los files
+									// tienen número mínimo de réplicas
+			optimalTime = mServersRequests[idServer].getTotalTime()
+					+ sServers.tranmissionTime(idServer, sRequests
+							.getRequest(i)[0]);
+			while (it.hasNext()) {
+				nextServer = it.next();
+				if (optimalTime > mServersRequests[nextServer].getTotalTime()
+						+ sServers.tranmissionTime(nextServer, sRequests
+								.getRequest(i)[0])) {
+					idServer = nextServer;
+					optimalTime = mServersRequests[nextServer].getTotalTime()
+							+ sServers.tranmissionTime(nextServer, sRequests
+									.getRequest(i)[0]);
+				}
+			}
+			// Asignarle el fichero
+			// canSwap es implícito al haber buscado un válido
+			swapOperator(idServer, i);
+		}
 	}
-	
+
+	public void initialRandomState(int seed) {
+		Set<Integer> serverSet;
+		Random rndGr = new Random(seed);
+		int rnd = -1;
+		int idFile = -1;
+		int idServer = -1;
+		Iterator<Integer> it;
+		for (int i = 0; i < sRequestsCount; ++i) {
+
+			idFile = sRequests.getRequest(i)[1];
+			// Obtener la lista de servidores con ese fichero
+			serverSet = sServers.fileLocations(idFile);
+
+			// Elegir aleatoriamente un servidor con el fichero
+			rnd = rndGr.nextInt(serverSet.size() + 1); // agregar caso sin
+														// servir
+			if (rnd < serverSet.size()) {
+				it = serverSet.iterator();
+				for (int j = 0; j <= rnd; ++j) {
+					idServer = it.next();
+				}
+				// Asignarle el fichero
+				// canSwap es implícito al haber buscado un válido
+				swapOperator(idServer, i);
+			}
+		}
+	}
+
 	public void initialGreedyState() {
-		
+		Set<Integer> serverSet;
+		int idFile = -1;
+		int idServer = -1;
+		int nextServer = -1;
+		int optimalTime = -1;
+
+		Iterator<Integer> it;
+		for (int i = 0; i < sRequestsCount; ++i) {
+			idFile = sRequests.getRequest(i)[1];
+			// Obtener la lista de servidores con ese fichero
+			serverSet = sServers.fileLocations(idFile);
+
+			// Elegir el servidor con menos carga
+			it = serverSet.iterator();
+			idServer = it.next(); // it tendrá mínimo un servidor, los files
+									// tienen número mínimo de réplicas
+			optimalTime = mServersRequests[idServer].getTotalTime()
+					+ sServers.tranmissionTime(idServer, sRequests
+							.getRequest(i)[0]);
+			while (it.hasNext()) {
+				nextServer = it.next();
+				if (optimalTime > mServersRequests[nextServer].getTotalTime()
+						+ sServers.tranmissionTime(nextServer, sRequests
+								.getRequest(i)[0])) {
+					idServer = nextServer;
+					optimalTime = mServersRequests[nextServer].getTotalTime()
+							+ sServers.tranmissionTime(nextServer, sRequests
+									.getRequest(i)[0]);
+				}
+			}
+			
+			if (optimalTime <= mServersRequests[idServer].getTotalTime()
+					+ sPenalization) {
+				// Asignarle el fichero
+				// canSwap es implícito al haber buscado un válido
+				swapOperator(idServer, i);
+			}
+		}
 	}
 
 	public void swapOperator(int idServer, int idRequest) {
-		
-		if (canRemove(idRequest)) removeOperator(idRequest);
+
+		if (canRemove(idRequest))
+			removeOperator(idRequest);
 		mRequestsServers[idRequest] = idServer;
-		//Agregar a la lista de requests del servidor actual el request y su tiempo
-		mServersRequests[idServer].addLink(idRequest, sServers.tranmissionTime(idServer, sRequests.getRequest(idRequest)[0]));
+		// Agregar a la lista de requests del servidor actual el request y su
+		// tiempo
+		mServersRequests[idServer].addLink(idRequest, sServers.tranmissionTime(
+				idServer, sRequests.getRequest(idRequest)[0]));
 	}
 
 	public boolean canSwap(int idServer, int idRequest) {
-		//Comprobar que la request existe
-		if (idRequest >= sRequestsCount) return false;
-		
-		//Comprobar que el servidor objetivo contiene el fichero(log n) (y se comprueba que el servidor existe por extensión)
-		if (!sServers.fileLocations(sRequests.getRequest(idRequest)[1]).contains(idServer)){
+		// Comprobar que la request existe
+		if (idRequest >= sRequestsCount)
+			return false;
+
+		// Comprobar que el servidor objetivo contiene el fichero(log n) (y se
+		// comprueba que el servidor existe por extensión)
+		if (!sServers.fileLocations(sRequests.getRequest(idRequest)[1])
+				.contains(idServer)) {
 			return false;
 		}
 		return true;
@@ -93,17 +198,18 @@ public class State {
 
 	public void removeOperator(int idRequest) {
 		int oldServer = mRequestsServers[idRequest];
-		mRequestsServers[idRequest]=-1;
-		//Eliminar de la lista de request del servidor actual el request
-		mServersRequests[oldServer].delLink(idRequest, sServers.tranmissionTime(oldServer, sRequests.getRequest(idRequest)[0]));
+		mRequestsServers[idRequest] = -1;
+		// Eliminar de la lista de request del servidor actual el request
+		mServersRequests[oldServer]
+				.delLink(idRequest, sServers.tranmissionTime(oldServer,
+						sRequests.getRequest(idRequest)[0]));
 	}
 
 	public boolean canRemove(int idRequest) {
-		//Comprobar que la request existe
-		if (idRequest >= sRequestsCount) return false;
-		return mRequestsServers[idRequest]==-1 ? false : true;
+		// Comprobar que la request existe
+		if (idRequest >= sRequestsCount)
+			return false;
+		return mRequestsServers[idRequest] == -1 ? false : true;
 	}
-
-
 
 }
