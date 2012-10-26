00dada4e1e0d74a179d770198214cfb1e36d0bdd
ivan.bargallo <ivan.bargallo@4c882253-0f09-8658-30a3-2cef7b2bf161>
2012-10-26 15:59:29 +0000

    nya

--- IA-1/src/StateSuccessorFunctionHill.java
+++ IA-1/src/StateSuccessorFunctionHill.java
@@ -7,6 +7,14 @@ public class StateSuccessorFunctionHill implements aima.search.framework.Success
 	@Override
 	public List getSuccessors(Object arg0) {
 		// TODO Auto-generated method stub
+		State padre = (State) arg0;
+		
+//		State n = new State(padre);
+//		if (n.canSwap(idServer, idRequest) {
+//			n.swapOperator(idServer, idRequest);
+//			list.add(n);
+//		}
+		
 		return null;
 	}
 
