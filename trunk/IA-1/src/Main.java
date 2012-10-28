e962982045d9014bcab806cd6b8b1be24e76af57
buiytre@gmail.com <buiytre@gmail.com@4c882253-0f09-8658-30a3-2cef7b2bf161>
2012-10-28 15:35:12 +0000

    ejercicio 3 sin medias

--- trunk/IA-1/src/Main.java
+++ trunk/IA-1/src/Main.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class Main {
 	public static void main(String args[]) {
 		experiment1(1);
-		//experiment2();
+		// experiment2();
 	}
 
 	private static void experiment1(int nTests) {
@@ -24,7 +24,7 @@ public class Main {
 		SuccessorFunction successorAlgorythm = null;
 		HeuristicFunction criteria = null;
 		SearchAgent agent = null;
-		
+
 		Date d1 = null;
 		Date d2 = null;
 
@@ -36,34 +36,38 @@ public class Main {
 		String[] operatorsMode = { "Modo 0", "Modo 1", "Modo 2", "Modo 3" };
 
 		// Establecer los parámetros del problema
-		//Primer criterio, con peticiones sin servir
+		// Primer criterio, con peticiones sin servir
 		State.setHeuristicMode("max");
 		criteria = new StateHeuristicFunction1();
 		State.setPenalizationTime(2500);
-		
+
 		System.out.println("##########\nEjercicio 1\n##########");
 		for (int i = 0; i < operatorsMode.length; ++i) {
-			System.out.println("##########\nModo operadores: " + operatorsMode[i] + "\n##########");
-			//El conjunto de operadores varía
+			System.out.println("##########\nModo operadores: "
+					+ operatorsMode[i] + "\n##########");
+			// El conjunto de operadores varía
 			State.setOperatorsMode(i);
-			
+
 			for (int j = 0; j < nTests; ++j) {
 				System.out.println("Prueba " + j + ":");
 				// El parámetro seed varía
 				State.setProblemParameters(nServers, nReplications, nUsers,
 						nRequestsUser, j);
 
-				// Generar el estado inicial (método generador greedy con peticiones sin servir)
+				// Generar el estado inicial (método generador greedy con
+				// peticiones sin servir)
 				d1 = new Date();
 				start = new State().greedyState();
-				
-				//Usamos Hill Climbing
+
+				// Usamos Hill Climbing
 				successorAlgorythm = new StateSuccessorFunctionHill();
 				search = new HillClimbingSearch();
-				//Primer criterio (minimización del servidor con más carga)
-				problem = new Problem(start, successorAlgorythm, new StateGoalTest(), criteria);
+				// Primer criterio (minimización del servidor con más carga)
+				problem = new Problem(start, successorAlgorythm,
+						new StateGoalTest(), criteria);
 				d2 = new Date();
-				System.out.println("Tiempo de generación: " + (d2.getTime() - d1.getTime()));
+				System.out.println("Tiempo de generación: "
+						+ (d2.getTime() - d1.getTime()));
 
 				// Ejecutar
 				d1 = new Date();
@@ -75,7 +79,8 @@ public class Main {
 				d2 = new Date();
 				System.out.println("Tiempo de ejecución: "
 						+ (d2.getTime() - d1.getTime()));
-				System.out.println("Iteraciones: " + search.getMetrics().getInt("nodesExpanded"));
+				System.out.println("Iteraciones: "
+						+ search.getMetrics().getInt("nodesExpanded"));
 				// Comparar heurísticas inicial y final
 				end = (State) search.getGoalState();
 				System.out.println("Heurística inicial: "
@@ -84,7 +89,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	private static void experiment2(int nTests) {
 		State start = null;
 		State end = null;
@@ -93,7 +98,7 @@ public class Main {
 		SuccessorFunction successorAlgorythm = null;
 		HeuristicFunction criteria = null;
 		SearchAgent agent = null;
-		
+
 		Date d1 = null;
 		Date d2 = null;
 
@@ -108,10 +113,11 @@ public class Main {
 		State.setHeuristicMode("max");
 		criteria = new StateHeuristicFunction1();
 		State.setOperatorsMode(0);
-		
+
 		System.out.println("##########\nEjercicio 2\n##########");
 		for (int i = 0; i < generatorFunc.length; ++i) {
-			System.out.println("Función generadora: " + generatorFunc[i] + "\n##########");
+			System.out.println("Función generadora: " + generatorFunc[i]
+					+ "\n##########");
 			for (int j = 0; j < nTests; ++j) {
 				System.out.println("Prueba " + j + ":");
 				// El parámetro seed varía
@@ -128,16 +134,18 @@ public class Main {
 					System.out.println("ERROR: FUNCIÓN GENERADORA INVÁLIDA");
 					break;
 				}
-				//Usamos Hill Climbing
+				// Usamos Hill Climbing
 				successorAlgorythm = new StateSuccessorFunctionHill();
 				search = new HillClimbingSearch();
-				//Primer criterio (minimización del servidor con más carga)
-				problem = new Problem(start, successorAlgorythm, new StateGoalTest(), criteria);
+				// Primer criterio (minimización del servidor con más carga)
+				problem = new Problem(start, successorAlgorythm,
+						new StateGoalTest(), criteria);
 				d2 = new Date();
 				System.out.println("Tiempo de generación: "
 						+ (d2.getTime() - d1.getTime()));
 
-				System.out.println("Iteraciones: " + agent.getInstrumentation().getProperty("Iterations"));
+				System.out.println("Iteraciones: "
+						+ agent.getInstrumentation().getProperty("Iterations"));
 				// Ejecutar
 				d1 = new Date();
 				try {
@@ -146,8 +154,10 @@ public class Main {
 					e.printStackTrace();
 				}
 				d2 = new Date();
-				System.out.println("Tiempo de ejecución: " + (d2.getTime() - d1.getTime()));
-				System.out.println("Iteraciones: " + search.getMetrics().getInt("nodesExpanded"));				
+				System.out.println("Tiempo de ejecución: "
+						+ (d2.getTime() - d1.getTime()));
+				System.out.println("Iteraciones: "
+						+ search.getMetrics().getInt("nodesExpanded"));
 				// Comparar heurísticas inicial y final
 				end = (State) search.getGoalState();
 				System.out.println("Heurística inicial: "
@@ -157,4 +167,94 @@ public class Main {
 		}
 	}
 
+	private static void experiment3(int nTests) {
+		State start = null;
+		State end = null;
+		Search search = null;
+		Problem problem = null;
+		SuccessorFunction successorAlgorythm = null;
+		HeuristicFunction criteria = null;
+		SearchAgent agent = null;
+		
+		Date d1 = null;
+		Date d2 = null;
+
+		int nServers = 50;
+		int nReplications = 5;
+		int nUsers = 200;
+		int nRequestsUser = 5;
+		int seed = 10;
+		String[] annealingParams = { "", "", "", "" };
+
+		// Parámetros de Simulated Annealing
+		int steps[] = {100000, 10000, 1000, 100};
+		int stiter[] = {100000, 10000, 1000, 100, 10, 1};
+		int k[] = {100000, 10000, 1000, 100, 10};
+		double lamb[] = {0.0001, 0.001, 0.01, 0.1, 1};
+		
+		// Establecer los parámetros del problema
+		// Primer criterio, sin peticiones sin servir
+		State.setHeuristicMode("max");
+		State.setOperatorsMode(0); // 0: Swap
+		criteria = new StateHeuristicFunction1();
+		System.out.println("##########\nEjercicio 3\n##########");
+		for (int loopSteps = 0; loopSteps < steps.length; ++loopSteps) {
+			for (int loopStiter = 0; loopStiter < stiter.length; ++loopStiter) {
+				for (int loopK = 0; loopK < k.length; ++loopK) {
+					//5 bucles anidados. He batido mi récord personal
+					for (int loopLamb = 0; loopLamb < lamb.length; ++loopLamb) {
+						System.out.println("##########");
+						System.out.print("steps=" + steps[loopSteps] + " ");
+						System.out.print("stiter=" + stiter[loopStiter] + " ");
+						System.out.print("k=" + k[loopK] + " ");
+						System.out.print("lamb=" + lamb[loopLamb]);
+						System.out.println("\n##########");
+						
+						for (int j = 0; j < nTests; ++j) {
+							System.out.println("Prueba " + j + ":");
+							// El parámetro seed varía
+							State.setProblemParameters(nServers, nReplications, nUsers, nRequestsUser, j);
+
+							// Generar el estado inicial (método generador
+							// greedy con peticiones sin servir)
+							d1 = new Date();
+							start = new State().greedyStateFullRequests();
+							
+							// Usamos Simulated Annealing
+							successorAlgorythm = new StateSuccessorFunctionSimulated();
+							search = new SimulatedAnnealingSearch(steps[loopSteps], 
+																	stiter[loopStiter], 
+																	k[loopK], 
+																	lamb[loopLamb]);
+							// Primer criterio (minimización del servidor con
+							// más carga)
+							problem = new Problem(start, successorAlgorythm, new StateGoalTest(), criteria);
+							d2 = new Date();
+							System.out.println("Tiempo de generación: " + (d2.getTime() - d1.getTime()));
+
+							// Ejecutar
+							d1 = new Date();
+							try {
+								agent = new SearchAgent(problem, search);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							d2 = new Date();
+							System.out.println("Tiempo de ejecución: "
+									+ (d2.getTime() - d1.getTime()));
+							System.out.println("Iteraciones: " + search.getMetrics().getInt("nodesExpanded"));
+							// Comparar heurísticas inicial y final
+							end = (State) search.getGoalState();
+							System.out.println("Heurística inicial: "
+									+ start.getHeuristic1());
+							System.out.println("Heurística final: " + end.getHeuristic1());
+						}
+
+					}
+				}
+			}
+		}
+		for (int i = 0; i < annealingParams.length; ++i) {
+					}
+	}
 }
